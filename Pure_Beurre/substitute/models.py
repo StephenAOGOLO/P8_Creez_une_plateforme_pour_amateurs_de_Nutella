223b6f19d9ce7d21e8a78f14a20a2ccc14971825
stephenAOGOLO <stephen.adams.ogolo@gmail.com>
2020-11-09 14:33:43 +0100

    12th commit - AlimentPage branch - Finishing DB Tables - Preparing/Formatting Data

--- Pure_Beurre/substitute/models.py
+++ Pure_Beurre/substitute/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Users(models.Model):
     name = models.CharField(max_length=100, null=True)
 
@@ -18,7 +19,12 @@ class Category(models.Model):
 
 class Aliment(models.Model):
     name = models.CharField(max_length=500, null=True)
-    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    category = models.ManyToManyField(Category)
+    brand = models.CharField(max_length=500, null=True)
+    nutriscore = models.CharField(max_length=1, null=True)
+    store = models.CharField(max_length=500, null=True)
+    purchase_places = models.CharField(max_length=500, null=True)
+    url = models.URLField(max_length=500, null=True)
 
     def __str__(self):
         return self.name
@@ -26,4 +32,8 @@ class Aliment(models.Model):
 
 class Historic(models.Model):
     user = models.ForeignKey(Users, null=True, on_delete=models.SET_NULL)
-    aliment = models.ForeignKey(Aliment, null=True, on_delete=models.SET_NULL)
+    aliment = models.ForeignKey(Aliment, related_name="aliment", null=True, on_delete=models.SET_NULL)
+    substitute = models.ForeignKey(Aliment,related_name="substitute", null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
