599a858e7155e3b42ba8c25634871033304ee657
stephenAOGOLO <stephen.adams.ogolo@gmail.com>
2020-12-19 14:59:15 +0100

    63th commit - master branch
    - Deploying project : IN PROGRESS
    - settings.py updated (Debug False + STATICFILES_DIRS) : DONE
    
    - Tests reporting : 96%
    - Adding unit tests and SELENIUM - DONE
    - Fixing top searchbar bug - DONE
    - Fixing front issues - DONE
    - Adding XSS handler - DONE
    - Enhancing URLS'page - DONE
    - Improving all pictures website - DONE
    - Adding more products into urls.json file - DONE
    - Adding animations - IN PROGRESS
    - Enhancing database search engine - IN PROGRESS
    - Updating README.md - IN PROGRESS
    - Updating TRELLO - IN PROGRESS
    - Designing MPD - IN PROGRESS
    - Updating admin console - IN PROGRESS
    - Cleaning code by pep8 - IN PROGRESS

--- Pure_Beurre/Pure_Beurre/settings.py
+++ Pure_Beurre/Pure_Beurre/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = tst.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["206.189.30.229"]
 
@@ -99,7 +99,7 @@ if DEBUG:
 
 
 else:
-# Production database config
+    # Production database config
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
