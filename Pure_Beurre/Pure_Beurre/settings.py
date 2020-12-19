65914576c86c7147a2f312b64ed1ac5770cff7cc
stephenAOGOLO <stephen.adams.ogolo@gmail.com>
2020-12-19 16:25:47 +0100

    82th commit - master branch
    - Deploying project : IN PROGRESS
    - settings.py updated (Debug False + STATIC_ROOT) : DONE
    
    - Tests reporting : 96%
    - Adding unit tests and SELENIUM - DONE
    - Fixing top searchbar bug - DONE
    - Fixing front issues - DONE
    - Adding XSS handler - DONE
    - Enhancing URLS'page - DONE
    - Improving all pictures website - DONE
    - Adding more products into urls.json file - DONE
    - Adding animations - IN PROGRESS
    - Enhancing database search engine - IN PROGRESS
    - Updating README.md - IN PROGRESS
    - Updating TRELLO - IN PROGRESS
    - Designing MPD - IN PROGRESS
    - Updating admin console - IN PROGRESS
    - Cleaning code by pep8 - IN PROGRESS

--- Pure_Beurre/Pure_Beurre/settings.py
+++ Pure_Beurre/Pure_Beurre/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'bloblo/static/'
+STATIC_URL = 'substitute/static/'
 #STATICFILES_FINDERS = [
 #    'django.contrib.staticfiles.finders.FileSystemFinder',
 #    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
