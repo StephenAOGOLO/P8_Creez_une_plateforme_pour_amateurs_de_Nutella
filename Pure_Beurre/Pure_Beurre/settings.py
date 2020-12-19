1e0db26f3bef4605003fe27ee42b2c4a747f8e4e
stephenAOGOLO <stephen.adams.ogolo@gmail.com>
2020-12-19 12:21:22 +0100

    46th commit - master branch
    - Deploying project : IN PROGRESS
    - settings.py updated (still fixing static path directory) : DONE
    
    - Tests reporting : 96%
    - Adding unit tests and SELENIUM - DONE
    - Fixing top searchbar bug - DONE
    - Fixing front issues - DONE
    - Adding XSS handler - DONE
    - Enhancing URLS'page - DONE
    - Improving all pictures website - DONE
    - Adding more products into urls.json file - DONE
    - Adding animations - IN PROGRESS
    - Enhancing database search engine - IN PROGRESS
    - Updating README.md - IN PROGRESS
    - Updating TRELLO - IN PROGRESS
    - Designing MPD - IN PROGRESS
    - Updating admin console - IN PROGRESS
    - Cleaning code by pep8 - IN PROGRESS

--- Pure_Beurre/Pure_Beurre/settings.py
+++ Pure_Beurre/Pure_Beurre/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "Pure_Beurre/substitute/static"), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "substitute/static"), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
